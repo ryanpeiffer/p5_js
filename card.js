4c5803f1fc5dc3ff124f1a659ceea1992be21706
Ryan Peiffer <ryanpeiff@gmail.com>
2020-04-16 21:31:50 -0500

    initial upload of blackjack sim

---
+++ card.js
@@ -0,0 +1,89 @@
+function Card(suit, value) {
+  this.suit = suit
+  this.text = value
+  if(value == 'A') {
+    this.value = 11 //placeholder, see function below for 1 vs 11 logic
+  } else if (value=='J' | value=='Q' | value=='K') {
+    this.value = 10
+  } else {
+    this.value = value
+  }
+
+  //values for formatting card
+  this.w = 50
+  this.h = (3/2) * this.w
+  this.corner = this.w / 5
+  this.textsize = 26
+
+  this.show = function(in_x, in_y) {
+    this.x = in_x
+    this.y = in_y
+    
+    //draw outline of card
+    stroke(0)
+    strokeWeight(1)
+    fill(255)
+    rectMode(CORNER)
+    rect(this.x, this.y, this.w, this.h, this.corner)      
+    //setup for drawing details of card
+    if(this.suit == 's' | this.suit == 'c') {
+      this.color = 0
+    } else {
+      this.color = [255,0,0]
+    }
+    fill(this.color)
+    noStroke()
+    textSize(this.textsize)
+    textAlign(CENTER)
+    
+    //draw suit
+    push()
+    translate(this.x + this.w/4,
+              this.y + this.h/2)
+    
+    if(this.suit == 'd') {
+      quad(-this.w/8, 0,
+           0, this.h/8,
+           this.w/8, 0,
+           0, -this.h/8)
+    } else if(this.suit == 'h') {
+      let size = this.w/4
+      translate(0, -4)
+      beginShape()
+      vertex(0, 0);
+      bezierVertex(-size / 2, -size / 2, - size, size / 3, 0, size);
+      bezierVertex(size, size / 3, size / 2, -size / 2, 0, 0);
+      endShape(CLOSE)
+    } else if (this.suit == 's') {
+      let size = this.w/90
+      let numSteps = 36
+	  translate(0, 2)
+      beginShape()
+	  for(var t=0; t<TWO_PI; t+=TWO_PI/numSteps) {
+	    var spadex = 16*pow(sin(t),3)
+		var spadey = 13*cos(t) - 5*cos(2*t) - 2*cos(3*t) - cos(4*t)
+		vertex(size*spadex, size*spadey)
+	  }
+	  endShape(CLOSE)
+    } else if (this.suit == 'c') {
+      let size = 8
+      ellipse(-size/2 + 0.5, 2, size, size)
+      ellipse(size/2 - 0.5, 2, size, size)
+      ellipse(0, -size/2 + 0.5, size, size)
+    }
+    pop()
+    
+    //write value on card
+    push()
+    translate(this.x + this.w/2 + 10,
+              this.y + this.h/2 + 10)
+    text(this.text, 0, 0)
+    pop()
+  }
+  
+  //call this function when we need to change an ace to a 1 instead of an 11
+  this.changeace = function() {
+    this.value = 1  
+  }
+  
+}
---
+++ sketch.js
@@ -0,0 +1,305 @@
+//inputs for gameplay
+let balance = 200
+let betsize = 5
+let n_decks = 6
+
+
+//variable definitions
+let card
+let values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+let suits = ['d', 'h', 'c', 's']
+let base_deck = []
+let deck = []
+
+let player_hand = []
+let player_count = 0
+let dealer_hand = []
+let dealer_count = 0
+
+let player_turn = false
+let player_blackjack = false
+let winloss = ' '
+let winloss_color = 0 
+
+let hit_button
+let stand_button
+let deal_button
+let bet_up_button
+let bet_down_button
+
+
+function setup() {
+  createCanvas(400, 400)
+  background([1, 67, 30])
+
+  hit_button = createButton('Hit')
+  hit_button.position(10, height / 2 - 40)
+  hit_button.mousePressed(hit)
+  hit_button.hide()
+
+  stand_button = createButton('Stand')
+  stand_button.position(hit_button.x, hit_button.y + 30)
+  stand_button.mousePressed(stand)
+  stand_button.hide()
+
+  deal_button = createButton('Deal')
+  deal_button.position(hit_button.x, hit_button.y + 60)
+  deal_button.mousePressed(deal)
+  
+  bet_up_button = createButton('+')
+  bet_up_button.position(315, 215)
+  bet_up_button.mousePressed(bet_up)
+  
+  bet_down_button = createButton('-')
+  bet_down_button.position(bet_up_button.x - 170, bet_up_button.y)
+  bet_down_button.mousePressed(bet_down)
+
+  //build a deck of cards
+  for (i = 0; i < suits.length; i++) {
+    for (j = 0; j < values.length; j++) {
+      card = new Card(suits[i], values[j])
+      base_deck.push(card)
+    }
+  }
+  shuffle_deck()
+}
+
+
+
+function draw() {
+  background([1, 67, 30])
+
+  //show player hand
+  for (i = 0; i < player_hand.length; i++) {
+    player_hand[i].show(10 + 50 * i, height - 95)
+  }
+
+  //show dealer hand
+  for (i = 0; i < dealer_hand.length; i++) {
+    dealer_hand[i].show(10 + 50 * i, 20)
+  }
+
+  //cover dealer's first card until player turn is finished
+  if (player_turn) {
+    push()
+    stroke(0)
+    strokeWeight(1)
+    fill([0, 126, 255]) 
+    rectMode(CORNER)
+    rect(dealer_hand[0].x, dealer_hand[0].y, dealer_hand[0].w, dealer_hand[0].h, dealer_hand[0].corner)
+    pop()
+  }
+
+  //write hand totals
+  push()
+  textSize(20)
+  stroke(40)
+  strokeWeight(1)
+  fill(240)
+  textAlign(LEFT, TOP)
+  text("Player count: " + player_count, 250, height - 50)
+  text("Dealer count: " + dealer_count, 250, 50)
+  pop()
+  
+  //box displaying balance, win/loss, etc
+  push()
+  translate(0.6 * width, 0.5 * height)
+  rectMode(CENTER)
+  fill(230)
+  stroke(0)
+  strokeWeight(2)
+  rect(0, 0, 0.7 * width, 0.4 * height)
+  
+  //translate to top middle of box
+  translate(0, -0.4 * height / 2)
+  textAlign(CENTER, TOP)
+  
+  //print current balance
+  textSize(16)
+  noStroke()
+  fill(0)
+  text('Current Balance: $' + balance, 0, 75)
+  
+  //print current betsize
+  text('Selected Bet: $' + betsize, 0, 100)
+  
+  //print win/loss status
+  textSize(28)
+  fill(winloss_color)
+  text(winloss, 0, 25)
+  
+  //print cards remaining in shoe
+  textSize(10)
+  fill(0)
+  text("Cards remaining in shoe: " + deck.length, -70, 0.4*height - 15)
+  pop()
+}
+
+function hit() {
+  if (player_turn) {
+    card = deck[deck.length - 1]
+    player_hand.push(card)
+    player_count += card.value
+    //logic to allow aces to be 1 or 11
+    for(i = 0; i < player_hand.length; i++) {
+      if(player_hand[i].text == 'A' &
+         player_count > 21) {
+        player_hand[i].changeace()
+        //need to recalculate player count after change
+        player_count = 0
+        for(j = 0; j < player_hand.length; j++) {
+          player_count += player_hand[j].value
+        }
+      }
+    }
+    deck.pop()
+  }
+
+  if (player_count > 21) {
+    dealer_count += dealer_hand[0].value
+    score()
+  }
+}
+
+function stand() {
+  //add the dealer's first card to their count
+  dealer_count += dealer_hand[0].value
+
+  while ((dealer_count < 17) &
+         (dealer_count < player_count)) {
+    card = deck[deck.length - 1]
+    dealer_hand.push(card)
+    dealer_count += card.value
+    //logic to allow aces to be 1 or 11
+    for(i = 0; i < dealer_hand.length; i++) {
+      if(dealer_hand[i].text == 'A' &
+         dealer_count > 21) {
+        dealer_hand[i].changeace()
+        //need to recalculate dealer count after change
+        dealer_count = 0
+        for(j = 0; j < dealer_hand.length; j++) {
+          dealer_count += dealer_hand[j].value
+        }
+      }
+    }
+    deck.pop()
+  }
+
+  score()
+}
+
+
+function shuffle_deck() {
+  //reset deck
+  deck = []
+
+  //duplicate base deck until we have n decks
+  for (i = 0; i < n_decks; i++) {
+    deck = deck.concat(base_deck)
+  }
+  shuffle(deck, true) //true modifies referenced array
+
+}
+
+
+function deal() {
+  //clear out existing hands
+  player_count = 0
+  dealer_count = 0
+  while (player_hand.length > 0) {
+    player_hand.pop()
+  }
+  while (dealer_hand.length > 0) {
+    dealer_hand.pop()
+  }
+  
+  //if deck < 10 cards, reshuffle before dealing
+  if(deck.length < 10) {
+    shuffle_deck()
+  }
+
+  //deal new starting hands
+  player_hand.push(deck[deck.length - 1])
+  deck.pop()
+  dealer_hand.push(deck[deck.length - 1])
+  deck.pop()
+  player_hand.push(deck[deck.length - 1])
+  deck.pop()
+  dealer_hand.push(deck[deck.length - 1])
+  deck.pop()
+
+  //calculate count for newly dealt hands
+  player_count += player_hand[0].value
+  player_count += player_hand[1].value
+  //don't add dealer's first card until player stands
+  dealer_count += dealer_hand[1].value
+  
+  //check if player was dealt a blackjack
+  if(player_count == 21) {
+    player_blackjack = true
+    dealer_count += dealer_hand[0].value
+    score()
+  } else {
+    //set things up for the player to take their turn
+    hit_button.show()
+    stand_button.show()
+    deal_button.hide()
+    bet_up_button.hide()
+    bet_down_button.hide()
+    player_turn = true
+    winloss = ' '
+  }
+
+}
+
+function score() {
+  player_turn = false
+  
+  if (player_blackjack) {
+    winloss = 'BLACKJACK!'
+    winloss_color = [255, 0, 255]
+    balance += betsize * 1.5
+  } else if (player_count > 21) {
+    winloss = 'You busted!'
+    winloss_color = [255, 0, 0]
+    balance -= betsize
+  } else if (dealer_count > 21) {
+    winloss = 'Dealer busted!'
+    winloss_color = [50, 168, 82]
+    balance += betsize
+  } else if (player_count < dealer_count) {
+    winloss = 'You lost!'
+    winloss_color = [255, 0, 0]
+    balance -= betsize
+  } else if (player_count == dealer_count) {
+    winloss = 'Push'
+    winloss_color = [0, 0, 0]
+  } else {
+    winloss = 'You win!'
+    winloss_color = [50, 168, 82]
+    balance += betsize
+  }
+  
+  //reset
+  player_blackjack = false
+  hit_button.hide()
+  stand_button.hide()
+  deal_button.show()
+  bet_up_button.show()
+  bet_down_button.show()
+  betsize = min(betsize, balance)
+}
+
+function bet_up() {
+  betsize += 5 
+  if(betsize > balance) {
+    betsize = balance
+  }
+}
+
+function bet_down() {
+ betsize -= 5
+  if(betsize < 5) {
+    betsize = min(balance, 5)
+  }
+}
